c56b500e451908d2b851cdea0f58292372618644
sakshiburghateNimap <sakshiburghate@nimapinfotech.com>
2022-11-30 16:54:09 +0530

    first commit

---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="row">
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <div class="col-md-12">
+            
+            <input type="text"  class="form-control" placeholder="Search" wire:model="searchTerm" />
+
+            <table class="table table-bordered" style="margin: 10px 0 10px 0;">
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                @foreach($users as $user)
+                <tr>
+                    <td>
+                        {{ $user->name }}
+                    </td>
+                    <td>
+                        {{ $user->email }}
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+            {{ $users->links() }}
+        </div>
+    </div>
+</div>
